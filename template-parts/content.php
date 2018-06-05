1c4950a27b67bca6b9753816127d122b108253d5
c.mcguire <cdmcguire000@gmail.com>
2018-06-05 16:45:10 -0700

    post date moved to footer

--- template-parts/content.php
+++ template-parts/content.php
@@ -17,16 +17,7 @@
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
-
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				muffuletta_posted_on();
-				muffuletta_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
+		?>
 	</header><!-- .entry-header -->
 
 	<?php muffuletta_post_thumbnail(); ?>
@@ -54,6 +45,16 @@
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
+		<?php
+		if ( 'post' === get_post_type() ) :
+			?>
+			<div class="entry-meta">
+				<?php
+					muffuletta_posted_on();
+					muffuletta_posted_by();
+				?>
+			</div><!-- .entry-meta -->
+		<?php endif; ?>
 		<?php muffuletta_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
